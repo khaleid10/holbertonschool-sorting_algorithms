64e808171d79b0dfd83cd41c229b450cb16dcece
khaleid10 <khaledaloraini@outlook.com>
2026-05-10 14:11:49 +0000

    Task 1: insertion sort list

--- sort.h
+++ sort.h
@@ -2,20 +2,23 @@
 #define SORT_H
 
 #include <stddef.h>
-#include <stdio.h>
 
 /**
- * print_array - prints an array
- * @array: array to print
- * @size: size of array
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
  */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
-
-/**
- * bubble_sort - sorts an array using bubble sort
- * @array: array to sort
- * @size: size of array
- */
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+
 #endif
