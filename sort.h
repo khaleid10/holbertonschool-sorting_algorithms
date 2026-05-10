e250ffb77ff8f19d9c7b428244c1654e469f7afb
khaleid10 <khaledaloraini@outlook.com>
2026-05-10 14:01:53 +0000

    Task 0: bubble sort

---
+++ sort.h
@@ -0,0 +1,21 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_array - prints an array
+ * @array: array to print
+ * @size: size of array
+ */
+void print_array(const int *array, size_t size);
+
+/**
+ * bubble_sort - sorts an array using bubble sort
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size);
+
+#endif
